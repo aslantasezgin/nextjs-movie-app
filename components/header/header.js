672e68f024f22517991a9da8b584dc381f77cd49
aslantasezgin <aslantasezginn@gmail.com>
2023-03-19 17:27:34 +0300

    header finished

--- components/header/header.js
+++ components/header/header.js
@@ -1,8 +1,27 @@
-import "./header.css"
+import {FaPlayCircle} from "react-icons/fa"
+import Link from "next/link"
+import styles from "../header/header.css"
 
 const Header = () => {
     return(
-        <div className="header container">header</div>
+        <div className="header">
+        <div className="container flex">
+        <div className="logoBox">
+        <Link href="/"><FaPlayCircle></FaPlayCircle> NETFILMS </Link>    
+        </div>
+
+        <div className="navList">
+         <nav>
+         <ul>
+         <li><Link href="/">Movies</Link></li>
+         <li><Link href="/">Series</Link></li>    
+         <li><Link href="/">Kids</Link></li>        
+        </ul>   
+        </nav>   
+        </div>
+
+        </div>
+        </div>
     )
 }
 
